1d0d9da9b0dc04c7827f2e314d186e6c5cd3318f
KatelynnT <49296698+KatelynnT@users.noreply.github.com>
2019-06-04 14:52:54 -0700

    Add files via upload

---
+++ mux.sv
@@ -0,0 +1,12 @@
+//Need to change bit size - I wasn't sure what it needed to be
+//If selector is 1, output = nes controller
+//If selector is 0, output = ps2 keyboard
+
+module mux(input logic[3:0] nes, ps2,
+				input logic selector,
+				output logic[3:0] output);
+				
+		assign output = selector ? nes : ps2;
+		
+endmodule
+
